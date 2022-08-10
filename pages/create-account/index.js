b0cc2aeb65eea8578744076817fb7a128a2052b0
Vincent Ochoa <100656411+vincentochoa3@users.noreply.github.com>
2022-08-10 13:08:57 -0700

    GS-8: Add a create account page (#2)
    * add a create account page

---
+++ pages/create-account/index.js
@@ -0,0 +1,11 @@
+import Heading1 from "../../components/Heading1/index";
+
+const CreateAccount = () => {
+  return (
+    <div>
+      <Heading1>Create Account!</Heading1>
+    </div>
+  );
+};
+
+export default CreateAccount;
