89ad33c2965673d97bf5bd20883fb8801696456a
sweetnectar <93552075+sweetnectar@users.noreply.github.com>
2022-08-10 16:48:25 -0700

    create signinform and begin styling

--- components/SignInForm/index.js
+++ components/SignInForm/index.js
@@ -1,5 +1,23 @@
-const SignInForm = ({ children }) => {
-  return <h1>{children}</h1>;
+import Heading1 from "../Heading1";
+import styles from "./styles.module.css";
+
+const SignInForm = () => {
+  return (
+    <form className={styles.container}>
+      <input type="text" className={styles.signInFields} placeholder="Email" />
+      <input
+        type="text"
+        className={styles.signInFields}
+        placeholder="Password"
+      />
+      <input type="submit" value="Sign In" className={styles.signInButton} />
+      <input
+        type="submit"
+        value="Sign In With Google"
+        className={styles.signInButton}
+      />
+    </form>
+  );
 };
 
 export default SignInForm;
--- pages/sign-in/index.js
+++ pages/sign-in/index.js
@@ -1,11 +1,13 @@
 import Heading1 from "../../components/Heading1";
+import styles from "./styles.module.css";
 import SignInForm from "../../components/SignInForm/index";
 
 const SignIn = () => {
   return (
-    <div>
-      <Heading1>Sign-in</Heading1>
+    <div className={styles.container}>
+      <Heading1>GLOW</Heading1>
       <SignInForm></SignInForm>
+      <p>Don't have an account? Sign Up</p>
     </div>
   );
 };
