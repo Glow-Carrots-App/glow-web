58c7c132e1c592ea0fdfea65be5eb2732c91ad54
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-08-10 13:00:27 -0700

    fix tabbing issue

--- pages/today/index.js
+++ pages/today/index.js
@@ -1,11 +1,11 @@
 import Heading1 from "../../components/Heading1/index";
 
 const Today = () => {
-	return (
-		<div>
-			<Heading1>Today</Heading1>
-		</div>
-	);
+  return (
+    <div>
+      <Heading1>Today!</Heading1>
+    </div>
+  );
 };
 
 export default Today;
