a2dc9324083632d81e73615ff89aaed564bb41b2
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-08-10 14:03:21 -0700

    add feedback subpage to settings page

---
+++ pages/settings/feedback/index.js
@@ -0,0 +1,11 @@
+import Heading1 from "../../../components/Heading1/index";
+
+const Feedback = () => {
+  return (
+    <div>
+      <Heading1>Feedback</Heading1>
+    </div>
+  );
+};
+
+export default Feedback;
