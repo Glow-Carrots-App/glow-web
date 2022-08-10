00291fb0307b936e97341baa6f4d982d8ae73ac2
Joel Colucci <joelcolucci@users.noreply.github.com>
2022-08-10 16:09:16 -0400

    GS-4: Add a signin page (#1)
    * add a signin page

---
+++ pages/sign-in/index.js
@@ -0,0 +1,11 @@
+import Heading1 from '../../components/Heading1';
+
+const SignIn = () => {
+  return (
+    <div>
+      <Heading1>Sign-in</Heading1>
+    </div>
+  );
+};
+
+export default SignIn;
