dedb98aa15c7743a1a01eca264db5d8ce4bee3b8
Robby <93552075+RobbyGardner@users.noreply.github.com>
2022-08-10 13:08:21 -0700

    GS-12: Create profile page (#4)
    * create a profile page

---
+++ pages/profile/index.js
@@ -0,0 +1,11 @@
+import Heading1 from "../../components/Heading1/index";
+
+const Profile = () => {
+  return (
+    <div>
+      <Heading1>Profile</Heading1>
+    </div>
+  );
+};
+
+export default Profile;
