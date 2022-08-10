e67db476a2dff46537581006a5f584fb5ffdefe5
Robby <93552075+RobbyGardner@users.noreply.github.com>
2022-08-10 14:19:03 -0700

    GS-57: Create password subpage (#11)
    * create password subpage

---
+++ pages/settings/password/index.js
@@ -0,0 +1,11 @@
+import Heading1 from "../../../components/Heading1/index";
+
+const Password = () => {
+  return (
+    <div>
+      <Heading1>Password</Heading1>
+    </div>
+  );
+};
+
+export default Password;
