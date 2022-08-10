ebbd881f4673eb070914436846a2c73214c71064
Robby <93552075+RobbyGardner@users.noreply.github.com>
2022-08-10 13:07:54 -0700

    GS-22: Create settings page (#5)
    * create settings page

---
+++ pages/settings/index.js
@@ -0,0 +1,11 @@
+import Heading1 from "../../components/Heading1/index.js";
+
+const Settings = () => {
+  return (
+    <div>
+      <Heading1>Settings</Heading1>
+    </div>
+  );
+};
+
+export default Settings;
