88e50456222118c8f7a68281472ca388dc5e8587
Vincent Ochoa <100656411+vincentochoa3@users.noreply.github.com>
2022-08-10 13:07:25 -0700

    GS-38: Add an eat page (#6)
    * add an eat page

---
+++ pages/eat/index.js
@@ -0,0 +1,11 @@
+import Heading1 from "../../components/Heading1/index";
+
+const Eat = () => {
+  return (
+    <div>
+      <Heading1>What are you eating</Heading1>
+    </div>
+  );
+};
+
+export default Eat;
