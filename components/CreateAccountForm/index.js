fff6428fcf7d29c5c39359d1e9363b7257f29795
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-08-10 15:47:51 -0700

    add styles to both create account page and its form component

--- components/CreateAccountForm/index.js
+++ components/CreateAccountForm/index.js
@@ -1,25 +1,17 @@
-// import React from "react"
+import styles from "./styles.module.css";
 
 const CreateAccountForm = () => {
   return (
-    <form>
-      <label>
-        First Name:
-        <input type="text" name="firstName" />
-      </label>
-      <label>
-        Email:
-        <input type="text" name="email" />
-      </label>
-      <label>
-        Password:
-        <input type="text" name="password" />
-      </label>
-      <label>
-        Confirm Password:
-        <input type="text" name="confirmPassword" />
-      </label>
-      <button>Sign Up</button>
+    <form className={styles.form}>
+      <input type="text" name="firstName" placeholder="First Name" />
+      <input type="text" name="email" placeholder="Email" />
+      <input type="text" name="password" placeholder="Password" />
+      <input
+        type="text"
+        name="confirmPassword"
+        placeholder="Confirm Password"
+      />
+      <input type="submit" value="Sign Up" />
     </form>
   );
 };
--- pages/create-account/index.js
+++ pages/create-account/index.js
@@ -1,11 +1,16 @@
 import Heading1 from "../../components/Heading1/index";
 import CreateAccountForm from "../../components/CreateAccountForm/index";
 
+import styles from "./styles.module.css";
+
 const CreateAccount = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <Heading1>Create Account!</Heading1>
       <CreateAccountForm />
+      <p>
+        Already have an account? <a>Sign in</a>
+      </p>
     </div>
   );
 };
