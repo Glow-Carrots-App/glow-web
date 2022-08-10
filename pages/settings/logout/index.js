eaa1ff265768e61e700a1c519110e8d1aee90fcf
Gino Ochoa <gochoa1088@gmail.com>
2022-08-10 13:54:39 -0700

    create a logout subpage

---
+++ pages/settings/logout/index.js
@@ -0,0 +1,11 @@
+import Heading1 from "../../../components/Heading1/index.js";
+
+const Logout = () => {
+  return (
+    <div>
+      <Heading1>Log Out</Heading1>
+    </div>
+  );
+};
+
+export default Logout;
