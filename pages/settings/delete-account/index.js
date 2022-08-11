3adf0f3609d70449ecc99ce3e128a954eadf5379
Gino Ochoa <gochoa1088@gmail.com>
2022-08-11 11:15:50 -0700

    create delete account component and apply styles

--- pages/settings/delete-account/index.js
+++ pages/settings/delete-account/index.js
@@ -1,11 +1,14 @@
 import Heading1 from "../../../components/Heading1/index.js";
+import DeleteAccount from "../../../components/DeleteAccount";
+import styles from "./styles.module.css";
 
-const DeleteAccount = () => {
+const Deleteaccount = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <Heading1>Delete Account</Heading1>
+      <DeleteAccount />
     </div>
   );
 };
 
-export default DeleteAccount;
+export default Deleteaccount;
