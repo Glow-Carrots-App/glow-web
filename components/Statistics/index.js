00efcb45a41bc05a7e45b979ecc9b0636dfaff7b
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-08-11 14:37:10 -0700

    add column structure to statistics component

---
+++ components/Statistics/index.js
@@ -0,0 +1,13 @@
+import styles from "./styles.module.css";
+
+const Statistics = () => {
+  return (
+    <div className={styles.container}>
+      <text className={styles.statsHeading}>Statistics</text>
+      <div>TOP ROW</div>
+      <div>BOTTOM ROW</div>
+    </div>
+  );
+};
+
+export default Statistics;
