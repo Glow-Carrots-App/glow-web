84d049bf398a2cc2561fd65987b0861924d168ec
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-08-11 15:14:43 -0700

    add the layout for Statistics component and reusable StatsCard component

--- components/Statistics/index.js
+++ components/Statistics/index.js
@@ -1,11 +1,19 @@
+import StatsCard from "../StatsCard";
+
 import styles from "./styles.module.css";
 
 const Statistics = () => {
   return (
     <div className={styles.container}>
       <text className={styles.statsHeading}>Statistics</text>
-      <div>TOP ROW</div>
-      <div>BOTTOM ROW</div>
+      <div className={styles.statsRow}>
+        <StatsCard title="DayStreak" />
+        <StatsCard title="Carrots" />
+      </div>
+      <div className={styles.statsRow}>
+        <StatsCard title="Highest Color" />
+        <StatsCard title="Lowest Color" />
+      </div>
     </div>
   );
 };
