8417a53d39ffdd312b36593ab72b110874ca789c
sweetnectar <93552075+sweetnectar@users.noreply.github.com>
2022-08-16 10:08:49 -0700

    add routing for signin

--- components/SignInForm/index.js
+++ components/SignInForm/index.js
@@ -1,13 +1,45 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+import { auth } from "../../firebase.js";
+
 import styles from "./styles.module.css";
 
 const SignInForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        router.push("/today");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+
   return (
-    <form className={styles.container} action="today" method="post">
-      <input type="text" className={styles.signInFields} placeholder="Email" />
+    <form className={styles.container} onSubmit={handleSignIn}>
+      <input
+        type="text"
+        className={styles.signInFields}
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
       <input
         type="password"
         className={styles.signInFields}
         placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
       <input type="submit" value="Sign In" className={styles.signInButton} />
 
