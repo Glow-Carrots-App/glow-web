19b77a9a8f3169f5917c4249a5f8369e387b576e
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-08-15 18:59:48 -0700

    add conditional auth testing to eat, profile, today, and settings pages

--- pages/eat/index.js
+++ pages/eat/index.js
@@ -1,11 +1,21 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 import Heading1 from "../../components/Heading1";
 import ColorFilter from "../../components/ColorFilter";
 import SearchFoodsList from "../../components/SearchFoodsList";
 import BottomTabs from "../../components/BottomTabs";
+import { useAuth } from "../../context/AuthContext";
 
 import styles from "./styles.module.css";
 
 const Eat = () => {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    user ? router.push("/eat") : router.push("sign-in");
+  }, []);
   return (
     <div className={styles.container}>
       <Heading1>What are you eating</Heading1>
--- pages/today/index.js
+++ pages/today/index.js
@@ -1,11 +1,21 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 import Heading1 from "../../components/Heading1";
 import TodayInfo from "../../components/TodayInfo";
 import WeeklyConsumption from "../../components/WeeklyConsumption";
 import BottomTabs from "../../components/BottomTabs";
+import { useAuth } from "../../context/AuthContext";
 
 import styles from "./styles.module.css";
 
 const Today = () => {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    user ? router.push("/today") : router.push("sign-in");
+  }, []);
   return (
     <div className={styles.container}>
       <Heading1>Today</Heading1>
