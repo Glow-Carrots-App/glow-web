1f5f17c655fee485f5922c552e5e8e3777d2becf
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-08-16 17:29:51 -0700

    deleted displayName from auth context

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -23,7 +23,6 @@ export const AuthContextProvider = ({ children }) => {
         setUser({
           uid: user.uid,
           email: user.email,
-          displayName: user.displayName,
         });
       } else {
         setUser(null);
