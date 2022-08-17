b885fe2e6ab94bedda429b87c30395ea318bac57
Gino Ochoa <gochoa1088@gmail.com>
2022-08-17 15:13:15 -0700

    settings page high fidelity update

--- components/SettingsInputs/index.js
+++ components/SettingsInputs/index.js
@@ -4,9 +4,9 @@ const SettingsInputs = () => {
   return (
     <form className={styles.container}>
       <label htmlFor="text">Name</label>
-      <input id="text" type="text" name="firstName" />
+      <input id="text" type="text" name="firstName" value="Robby" />
       <label htmlFor="email">Email</label>
-      <input id="email" type="text" name="email" />
+      <input id="email" type="text" name="email" value="vince@gmail.com" />
     </form>
   );
 };
