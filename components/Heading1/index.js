a5c64123cd7d020ad0b4dcc4e873920b11aa8f81
Gino Ochoa <gochoa1088@gmail.com>
2022-08-17 11:15:14 -0700

    heading1 and heading2 css updates

--- components/Heading1/index.js
+++ components/Heading1/index.js
@@ -1,7 +1,7 @@
+import styles from "./styles.module.css";
+
 const Heading1 = ({ children }) => {
-  return (
-    <h1>{children}</h1>
-  );
+  return <h1 className={styles.heading1}>{children}</h1>;
 };
 
 export default Heading1;
