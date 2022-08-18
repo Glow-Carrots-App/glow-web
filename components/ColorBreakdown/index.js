b90143600bf7778daf25cbb839bc8bcd459ebe2e
Gino Ochoa <gochoa1088@gmail.com>
2022-08-17 18:38:28 -0700

    implemented self made chart with basic dummy data

--- components/ColorBreakdown/index.js
+++ components/ColorBreakdown/index.js
@@ -1,10 +1,41 @@
 import styles from "./styles.module.css";
 
 const ColorBreakdown = () => {
+  let colorArr = [
+    "#f94d4d",
+    "#f94d4d",
+    "#f94d4d",
+    "#f94d4d",
+    "#fd8f52",
+    "#fd8f52",
+    "#f9c449",
+    "#f9c449",
+    "#f9c449",
+    "#6aab9c",
+    "#6aab9c",
+    "#6aab9c",
+    "#6aab9c",
+    "#6aab9c",
+    "#4b37a8",
+    "#4b37a8",
+    "#4b37a8",
+    "#4b37a8",
+    "#4b37a8",
+    "#4b37a8",
+    "#f2edcf",
+  ];
   return (
     <div className={styles.container}>
       <p>Total Color Breakdown</p>
-      <div className={styles.progressBar}>Progress Bar</div>
+      <div className={styles.progressBar}>
+        {colorArr.map((color, index, arr) => (
+          <div
+            className={styles.progressItem}
+            style={{ backgroundColor: color, width: 500 / arr.length }}
+            key={index + color}
+          />
+        ))}
+      </div>
     </div>
   );
 };
