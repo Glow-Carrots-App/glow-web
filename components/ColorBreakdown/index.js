e067a575972b62b584e77df3952102b7ce5b1f81
Gino Ochoa <gochoa1088@gmail.com>
2022-08-18 10:55:08 -0700

    color breakdown rework to fix large amount of possible divs

--- components/ColorBreakdown/index.js
+++ components/ColorBreakdown/index.js
@@ -1,41 +1,47 @@
+import countColors from "../../utils/countColors";
+
 import styles from "./styles.module.css";
 
 const ColorBreakdown = () => {
   let colorArr = [
-    "#f94d4d",
-    "#f94d4d",
-    "#f94d4d",
-    "#f94d4d",
-    "#fd8f52",
-    "#fd8f52",
-    "#f9c449",
-    "#f9c449",
-    "#f9c449",
-    "#6aab9c",
-    "#6aab9c",
-    "#6aab9c",
-    "#6aab9c",
-    "#6aab9c",
-    "#4b37a8",
-    "#4b37a8",
-    "#4b37a8",
-    "#4b37a8",
-    "#4b37a8",
-    "#4b37a8",
-    "#f2edcf",
+    { color: "red", fillHex: "#f94d4d" },
+    { color: "red", fillHex: "#f94d4d" },
+    { color: "red", fillHex: "#f94d4d" },
+    { color: "red", fillHex: "#f94d4d" },
+    { color: "orange", fillHex: "#fd8f52" },
+    { color: "orange", fillHex: "#fd8f52" },
+    { color: "yellow", fillHex: "#f9c449" },
+    { color: "yellow", fillHex: "#f9c449" },
+    { color: "yellow", fillHex: "#f9c449" },
+    { color: "green", fillHex: "#6aab9c" },
+    { color: "green", fillHex: "#6aab9c" },
+    { color: "green", fillHex: "#6aab9c" },
+    { color: "green", fillHex: "#6aab9c" },
+    { color: "green", fillHex: "#6aab9c" },
+    { color: "blue", fillHex: "#4b37a8" },
+    { color: "blue", fillHex: "#4b37a8" },
+    { color: "blue", fillHex: "#4b37a8" },
+    { color: "blue", fillHex: "#4b37a8" },
+    { color: "blue", fillHex: "#4b37a8" },
+    { color: "blue", fillHex: "#4b37a8" },
+    { color: "tan", fillHex: "#f2edcf" },
   ];
 
-  let percentage = 100 / colorArr.length + "%";
+  let colorCount = countColors(colorArr);
+  let percentages = colorCount.map(
+    (color) => (100 * color.count) / colorCount[6].count + "%"
+  );
+  colorCount.forEach((count, index) => (count.percentage = percentages[index]));
 
   return (
     <div className={styles.container}>
       <p>Total Color Breakdown</p>
       <div className={styles.progressBar}>
-        {colorArr.map((color, index) => (
+        {colorCount.map((color, index) => (
           <div
+            key={color + index}
             className={styles.progressItem}
-            style={{ backgroundColor: color, width: percentage }}
-            key={index + color}
+            style={{ backgroundColor: color.fillHex, width: color.percentage }}
           />
         ))}
       </div>
