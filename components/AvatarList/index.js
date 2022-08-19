b05d3307d2e5a91b9e05c3f83699a88a0d53e14c
sweetnectar <93552075+sweetnectar@users.noreply.github.com>
2022-08-19 15:34:02 -0700

    Highlight selected avatar gold

--- components/AvatarList/index.js
+++ components/AvatarList/index.js
@@ -1,10 +1,14 @@
+import { useState } from "react";
+
 import SmallLinkedButton from "../SmallLinkedButton";
 
 import styles from "./styles.module.css";
 
 const AvatarList = () => {
+  const [selectedAvatar, setSelectedAvatar] = useState("/avatars/salad.png");
+
   const avatars1 = [
-    "/avatars/baby-food.png",
+    "/avatars/salad.png",
     "/avatars/cooking-pot.png",
     "/avatars/food-container.png",
     "/avatars/pie.png",
@@ -14,24 +18,41 @@ const AvatarList = () => {
   const avatars2 = [
     "/avatars/noodle.png",
     "/avatars/camp.png",
-    "/avatars/salad.png",
+    "/avatars/baby-food.png",
     "/avatars/leftover.png",
     "/avatars/birthday-cake.png",
     "/avatars/tropical-fruit.png",
   ];
+
   return (
     <div className={styles.container}>
       <div className={styles.avatarList}>
         <div>
           {avatars1.map((avatar, index) => (
-            <div className={styles.avatarListItem} key={avatar + index}>
+            <div
+              className={
+                avatar === selectedAvatar
+                  ? styles.avatarListItemSelected
+                  : styles.avatarListItem
+              }
+              key={avatar + index}
+              onClick={() => setSelectedAvatar(avatar)}
+            >
               <img src={avatar} className={styles.avatar} />
             </div>
           ))}
         </div>
         <div className={styles.avatarColumn}>
           {avatars2.map((avatar, index) => (
-            <div className={styles.avatarListItem} key={avatar + index}>
+            <div
+              className={
+                avatar === selectedAvatar
+                  ? styles.avatarListItemSelected
+                  : styles.avatarListItem
+              }
+              key={avatar + index}
+              onClick={() => setSelectedAvatar(avatar)}
+            >
               <img src={avatar} className={styles.avatar} />
             </div>
           ))}
