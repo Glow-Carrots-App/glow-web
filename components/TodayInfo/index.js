9506c0d8daae590f466cb21c8a92befb7b4be35a
Gino Ochoa <gochoa1088@gmail.com>
2022-08-22 17:45:40 -0700

    golden carrot and today list fix

--- components/TodayInfo/index.js
+++ components/TodayInfo/index.js
@@ -1,6 +1,7 @@
 import { VictoryPie } from "victory";
 
 import populateDonutChartData from "../../utils/populateDonutChartData";
+import TODAY_SAMPLE_FOOD_DATA from "../../sampleData/userSampleFoodData";
 
 import styles from "./styles.module.css";
 
@@ -14,23 +15,16 @@ const TodayInfo = () => {
     "#9a7dcc",
     "#d1d0d0",
   ];
-  const todayEaten = [
-    { product: "Strawberry", color: "red", fillHex: "#f94d4d" },
-    { product: "Strawberry", color: "red", fillHex: "#f94d4d" },
-    { product: "Banana", color: "yellow", fillHex: "#ffbb1c" },
-    { product: "Orange", color: "orange", fillHex: "#fd8f52" },
-    { product: "Banana", color: "yellow", fillHex: "#ffbb1c" },
-    { product: "Onion", color: "white", fillHex: "#97906e" },
-    { product: "Scallions", color: "green", fillHex: "#479d45" },
-    { product: "Blueberry", color: "blue", fillHex: "#7471e7" },
-  ];
 
-  let currentCount = todayEaten.length;
+  let currentCount = TODAY_SAMPLE_FOOD_DATA.length;
   let user = {
     dailyGoal: 8,
   };
 
-  const todaysData = populateDonutChartData(todayEaten, user.dailyGoal);
+  const todaysData = populateDonutChartData(
+    TODAY_SAMPLE_FOOD_DATA,
+    user.dailyGoal
+  );
 
   return (
     <div className={styles.container}>
@@ -52,8 +46,8 @@ const TodayInfo = () => {
       </div>
       <div className={styles.listContainer}>
         <ul className={styles.foodList}>
-          {todayEaten.map((food, index) => (
-            <li style={{ color: food.fillHex }} key={index + food.product}>
+          {TODAY_SAMPLE_FOOD_DATA.map((food, index) => (
+            <li style={{ color: food.textHex }} key={index + food.product}>
               {food.product}
             </li>
           ))}
