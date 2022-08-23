b8b3daad2511e7836b9a76f735d633fb71ca275f
Gino Ochoa <gochoa1088@gmail.com>
2022-08-23 14:15:53 -0700

    implement profile responsive tablet design

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,7 +1,3 @@
-import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGear } from "@fortawesome/free-solid-svg-icons";
-
 import Heading1 from "../../components/Heading1";
 import UserInfo from "../../components/UserInfo";
 import Statistics from "../../components/Statistics";
@@ -13,11 +9,6 @@ import styles from "./styles.module.css";
 const ProfilePage = () => {
   return (
     <div className={styles.container}>
-      <Link href="/settings">
-        <a className={styles.settingsBtn}>
-          <FontAwesomeIcon icon={faGear} size="xl" color="#9084ab" />
-        </a>
-      </Link>
       <Heading1>Profile</Heading1>
       <UserInfo />
       <Statistics />
