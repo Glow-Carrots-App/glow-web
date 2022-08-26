301e692b8e154d7d44d756fff4afa90eb52c3a45
sweetnectar <93552075+sweetnectar@users.noreply.github.com>
2022-08-26 16:20:17 -0700

    Work in progress

--- components/ChangePasswordForm/index.js
+++ components/ChangePasswordForm/index.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
 import { useAuth } from "../../context/AuthContext";
 
 import styles from "./styles.module.css";
 
 const ChangePasswordForm = () => {
+  const [oldPassword, setOldPassword] = useState();
+  const [newPassword, setNewPassword] = useState();
+  const [confirmNewPassword, setConfirmNewPassword] = useState();
+
   const {
     authedUser: { uid },
+    changePassword,
   } = useAuth();
+
+  const handlePasswordUpdate = (e) => {
+    e.preventDefault();
+    changePassword(newPassword);
+    //change style
+  };
 
   return (
     <form
@@ -18,29 +31,39 @@ const ChangePasswordForm = () => {
         className={styles.passwordFields}
         type="password"
         name="oldPassword"
+        value={oldPassword}
         placeholder="Old Password"
+        onChange={(e) => setOldPassword(e.target.value)}
       />
       <input
         className={styles.passwordFields}
         type="password"
         name="newPassword"
+        value={newPassword}
         placeholder="New Password"
+        onChange={(e) => setNewPassword(e.target.value)}
       />
       <input
         className={styles.passwordFields}
         type="password"
         name="confirmNewPassword"
+        value={confirmNewPassword}
         placeholder="Confirm New Password"
+        onChange={(e) => setConfirmNewPassword(e.target.value)}
       />
       <div className={styles.passwordButtonPair}>
         <button type="reset" className={styles.resetButton}>
           Reset
         </button>
         <input
+          // className={
+          //   //if new password added to FB, change color
+          //   //else stay same style
+          // }
           className={styles.passwordButton}
           type="submit"
-          name="savePassword"
           value="Save"
+          onClick={handlePasswordUpdate}
         />
       </div>
     </form>
