630efb73701a56f5a020c992991cb5258fb011fc
gochoa1088 <gochoa1088@gmail.com>
2022-08-27 19:51:49 -0700

    abort fetching component bug fix related to redirect from logout/delete account

--- components/DeleteAccount/index.js
+++ components/DeleteAccount/index.js
@@ -1,5 +1,3 @@
-import { useRouter } from "next/router";
-
 import { useAuth } from "../../context/AuthContext";
 import UserModel from "../../model/user";
 import FoodEntryModel from "../../model/foodEntry";
@@ -13,7 +11,6 @@ const DeleteAccount = () => {
     deleteAccount,
     authedUser: { uid },
   } = useAuth();
-  const router = useRouter();
 
   const handleDelete = async (e) => {
     e.preventDefault();
@@ -22,7 +19,6 @@ const DeleteAccount = () => {
       await deleteAccount();
       await UserModel.deleteUser(uid);
       await FoodEntryModel.deleteUserHistory(uid);
-      router.push("/sign-in");
     } catch (err) {
       console.log(err);
     }
@@ -33,9 +29,9 @@ const DeleteAccount = () => {
       <Heading2>Are you sure you want to delete your account?</Heading2>
       <div className={styles.buttonPair}>
         <SmallLinkedButton href="/settings">Cancel</SmallLinkedButton>
-        <a className={styles.smallButton} onClick={handleDelete}>
+        <button className={styles.smallButton} onClick={handleDelete}>
           Delete Account
-        </a>
+        </button>
       </div>
     </div>
   );
--- components/LogOut/index.js
+++ components/LogOut/index.js
@@ -1,5 +1,3 @@
-import { useRouter } from "next/router";
-
 import Heading2 from "../Heading2";
 import SmallLinkedButton from "../SmallLinkedButton";
 import { useAuth } from "../../context/AuthContext";
@@ -8,14 +6,12 @@ import styles from "./styles.module.css";
 
 const LogOut = () => {
   const { authedUser, logout } = useAuth();
-  const router = useRouter();
 
   const handleLogOut = async (e) => {
     e.preventDefault();
 
     try {
       await logout(authedUser);
-      router.push("/sign-in");
     } catch (err) {
       console.log(err);
     }
@@ -26,9 +22,9 @@ const LogOut = () => {
       <Heading2>Are you sure you want to log out?</Heading2>
       <div className={styles.buttonPair}>
         <SmallLinkedButton href="/settings">Cancel</SmallLinkedButton>
-        <a className={styles.smallButton} onClick={handleLogOut}>
+        <button className={styles.smallButton} onClick={handleLogOut}>
           Log Out
-        </a>
+        </button>
       </div>
     </div>
   );
