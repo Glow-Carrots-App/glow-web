951122636ae7eebf52f9ddb3f788a983ae80a313
Gino Ochoa <gochoa1088@gmail.com>
2022-08-29 16:31:34 -0700

    create account form validation

---
+++ utils/validateEmail.js
@@ -0,0 +1,4 @@
+export const validateEmailRegex =
+  // prettier-ignore
+  "^[a-z0-9]+@[a-z]+\.[a-z]{2,3}";
+export const validateEmailMsg = "Invalid email format.";
--- utils/validatePassword.js
+++ utils/validatePassword.js
@@ -1,4 +1,4 @@
 export const validatePasswordRegex =
-  "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,}$";
+  "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
 export const validatePasswordMsg =
   "Password must be minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character.";
