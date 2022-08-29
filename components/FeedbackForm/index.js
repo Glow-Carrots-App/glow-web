68bef2a1077a1a46df6b21de4befae5a17c4bc58
sweetnectar <93552075+sweetnectar@users.noreply.github.com>
2022-08-29 15:02:26 -0700

    Add code relating to emailJS

--- components/FeedbackForm/index.js
+++ components/FeedbackForm/index.js
@@ -1,3 +1,6 @@
+import { useRef } from "react";
+import emailjs from "emailjs-com";
+
 import { useAuth } from "../../context/AuthContext";
 
 import styles from "./styles.module.css";
@@ -7,10 +10,28 @@ const FeedbackForm = () => {
     authedUser: { uid, email },
   } = useAuth();
 
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_USER_ID
+      )
+      .then(
+        (result) => console.log(result.text),
+        (error) => console.log(error.text)
+      );
+    e.target.reset();
+  };
   return (
-    <form className={styles.container} action="/settings" mode="post">
+    <form className={styles.container} ref={form} onSubmit={sendEmail}>
       <label htmlFor="subject">Subject</label>
       <input
+        required
         id="subject"
         type="text"
         name="subjectText"
@@ -18,6 +39,7 @@ const FeedbackForm = () => {
       />
       <label htmlFor="message">Message</label>
       <textarea
+        required
         id="message"
         type="text"
         name="messageText"
