69e81ccf2b136f3c7ab8eb8f292462bb456520cd
sweetnectar <93552075+sweetnectar@users.noreply.github.com>
2022-08-31 16:03:04 -0700

    Add responsive design for about page

---
+++ components/About/index.js
@@ -0,0 +1,23 @@
+import styles from "./styles.module.css";
+
+const About = () => {
+  return (
+    <div className={styles.container}>
+      <p className={styles.text}>
+        Glow is a food tracker app inspired by the Rainbow Diet and science
+        linking nutrition to the colors our food.
+      </p>
+      <p className={styles.text}>
+        This app is intended for all ages. It is not indended to treat,
+        diagnose, prevent, or cure diseases.
+      </p>
+      <p className={styles.text}>
+        Icons are made by
+        <a href="https://www.flaticon.com/authors/freepik"> Freepik</a> from
+        <a href="https://www.flaticon.com/"> www.flaticon.com</a>.
+      </p>
+    </div>
+  );
+};
+
+export default About;
