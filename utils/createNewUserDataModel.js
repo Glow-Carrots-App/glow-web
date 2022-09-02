1dc2c05c911017815fd8b043314be9ee3fffc1df
sweetnectar <93552075+sweetnectar@users.noreply.github.com>
2022-09-02 16:26:08 -0700

    Flatten dailyGoal object

--- utils/createNewUserDataModel.js
+++ utils/createNewUserDataModel.js
@@ -3,16 +3,14 @@ import dayjs from "dayjs";
 export default function createNewUserDataModel(email, firstName, uid) {
   const newUser = {
     avatar: "/avatars/camp.png",
-    dailyGoal: {
-      amount: 5,
-      isComplete: false,
-      lastGoalDate: dayjs().format("YYYY/MM/DD"),
-    },
+    dailyGoalAmount: 5,
     dayStreak: 0,
     email,
     firstName,
     goldenCarrots: 0,
+    isDailyGoalComplete: false,
     joinDate: dayjs().format("MM/DD/YYYY"),
+    lastGoalDate: dayjs().format("YYYY/MM/DD"),
     uid,
   };
   return newUser;
