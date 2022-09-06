4b84842ba086a9999c50eef0e89bcdf704cdf15d
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-09-06 16:39:48 -0700

    add tests for both cases in check lowest color util

---
+++ utils/__tests__/checkLowestColor.test.js
@@ -0,0 +1,63 @@
+import checkLowestColor from "../checkLowestColor";
+
+const totalFoods = [
+  {
+    product: "Cabbage",
+    productSearch: "Cabbage (Green)",
+    color: "White",
+    textHex: "#479D45",
+    fillHex: "#6AAB9C",
+    date: null,
+  },
+  {
+    product: "Cabbage",
+    productSearch: "Cabbage (Green)",
+    color: "Green",
+    textHex: "#479D45",
+    fillHex: "#6AAB9C",
+    date: null,
+  },
+  {
+    product: "Cabbage",
+    productSearch: "Cabbage (Green)",
+    color: "Yellow",
+    textHex: "#479D45",
+    fillHex: "#6AAB9C",
+    date: null,
+  },
+  {
+    product: "Cabbage",
+    productSearch: "Cabbage (Green)",
+    color: "Green",
+    textHex: "#479D45",
+    fillHex: "#6AAB9C",
+    date: null,
+  },
+  {
+    product: "Lettuce",
+    productSearch: "Lettuce (Green)",
+    color: "Orange",
+    textHex: "#479D45",
+    fillHex: "#6AAB9C",
+    date: null,
+  },
+  {
+    product: "Arugula",
+    productSearch: "Arugula",
+    color: "Red",
+    textHex: "#479D45",
+    fillHex: "#6AAB9C",
+    date: null,
+  },
+];
+
+describe("checkLowestColor", () => {
+  test("return lowest color object", () => {
+    const testRun = checkLowestColor(totalFoods);
+    expect(testRun.color).toBe("Purple");
+  });
+  test("return empty string when no foods present", () => {
+    const testRun = checkLowestColor([]);
+    expect(testRun.color).toBe("");
+  });
+});
