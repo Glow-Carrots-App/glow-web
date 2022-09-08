27d0bf02019168f4f4d2d85999fa5f4178dc7e8d
sweetnectar <93552075+sweetnectar@users.noreply.github.com>
2022-09-08 14:32:07 -0700

    Add tests for TodayUserInfo component

---
+++ components/TodayUserInfo/index.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodayUserInfo from ".";
+
+describe("TodayUserInfo", () => {
+  const mockedUser = {
+    avatar: "/avatars/camp.png",
+    firstName: "Vince",
+    joinDate: "08/12/2022",
+  };
+  beforeEach(() => render(<TodayUserInfo user={mockedUser} />));
+  it("should render the correct avatar", () => {
+    const imgElement = screen.getByRole("img");
+    expect(imgElement).toBeInTheDocument();
+  });
+  it("should render the correct name", () => {
+    const firstNameTextElement = screen.getByText("Vince");
+    expect(firstNameTextElement).toBeInTheDocument();
+  });
+  it("should render the correct join date", () => {
+    const dateElement = screen.getByText("Joined Aug 2022");
+    expect(dateElement).toBeInTheDocument();
+  });
+});
