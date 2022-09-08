5d25988dd030774c9e42df8fd70303ef8a930a3c
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-09-08 15:37:32 -0700

    add testing for eat foods list component

---
+++ components/EatFoodsList/index.test.js
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import EatFoodsList from ".";
+
+const mockedData = [
+  {
+    productSearch: "Cabbage (Green)",
+  },
+  {
+    productSearch: "Blackberry",
+  },
+  {
+    productSearch: "Strawberry",
+  },
+  {
+    productSearch: "Lemon",
+  },
+  {
+    productSearch: "Apricot (Orange)",
+  },
+];
+
+describe("EatFoodsList", () => {
+  beforeEach(() => render(<EatFoodsList data={mockedData} />));
+
+  it("should render a container div for food list", () => {
+    const containerElement = screen.getByRole("container");
+    expect(containerElement).toBeInTheDocument();
+  });
+
+  it("should render a div for a food list item", () => {
+    const listElement = screen.getByText(/cabbage/i);
+    expect(listElement).toBeInTheDocument();
+  });
+
+  it("should render a div for a food list item", () => {
+    const listElements = screen.getAllByRole("food-list-item");
+    expect(listElements.length).toBe(5);
+  });
+});
