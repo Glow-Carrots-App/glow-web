6d244fd0492ae74753a107f6298169e3012b148f
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-09-08 15:39:39 -0700

    correct description for test

--- components/EatFoodsList/index.test.js
+++ components/EatFoodsList/index.test.js
@@ -29,12 +29,12 @@ describe("EatFoodsList", () => {
     expect(containerElement).toBeInTheDocument();
   });
 
-  it("should render a div for a food list item", () => {
+  it("should render text for a food list item", () => {
     const listElement = screen.getByText(/cabbage/i);
     expect(listElement).toBeInTheDocument();
   });
 
-  it("should render a div for a food list item", () => {
+  it("should render 5 divs for each food list item", () => {
     const listElements = screen.getAllByRole("food-list-item");
     expect(listElements.length).toBe(5);
   });
