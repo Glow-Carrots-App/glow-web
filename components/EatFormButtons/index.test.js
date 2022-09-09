13c7f596acade7ff97e0fb81d311b21fd2694b66
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-09-09 13:51:10 -0700

    add necessary tests

--- components/EatFormButtons/index.test.js
+++ components/EatFormButtons/index.test.js
@@ -6,28 +6,56 @@ const MOCKED_CURRENT_DAY = [{}, {}, {}];
 const MOCKED_USER = { isGoalComplete: false, uid: "fake_id" };
 
 describe("EatFormButtons", () => {
-  beforeEach(() =>
-    render(
-      <EatFormButtons
-        currentDay={MOCKED_CURRENT_DAY}
-        selectedFood={{}}
-        user={MOCKED_USER}
-      />
-    )
-  );
+  describe("add button", () => {
+    it("should render the Add button that is disabled", () => {
+      render(
+        <EatFormButtons
+          currentDay={MOCKED_CURRENT_DAY}
+          selectedFood={null}
+          user={MOCKED_USER}
+        />
+      );
+      const addButtonElement = screen.getByText(/add/i);
+      expect(addButtonElement).toBeInTheDocument();
+      expect(addButtonElement).toBeDisabled;
+    });
 
-  it("should render the Add button", () => {
-    const addButtonElement = screen.getByText(/add/i);
-    expect(addButtonElement).toBeInTheDocument();
+    it("should render enabled button when selectedFood", () => {
+      render(
+        <EatFormButtons
+          currentDay={MOCKED_CURRENT_DAY}
+          selectedFood={{ product: "Apple" }}
+          user={MOCKED_USER}
+        />
+      );
+      const addButtonElement = screen.getByText(/add/i);
+      expect(addButtonElement).not.toBeDisabled();
+    });
   });
 
-  it("should render the Cancel Button", () => {
-    const cancelButtonElement = screen.getByText(/cancel/i);
-    expect(cancelButtonElement).toBeInTheDocument();
-  });
+  describe("cancel button", () => {
+    it("should render the Cancel Button", () => {
+      render(
+        <EatFormButtons
+          currentDay={MOCKED_CURRENT_DAY}
+          selectedFood={null}
+          user={MOCKED_USER}
+        />
+      );
+      const cancelButtonElement = screen.getByText(/cancel/i);
+      expect(cancelButtonElement).toBeInTheDocument();
+    });
 
-  it("should have href attribute and should be /today", () => {
-    const linkElement = screen.getByRole("link");
-    expect(linkElement).toHaveAttribute("href", "/today");
+    it("should have href attribute and should be /today", () => {
+      render(
+        <EatFormButtons
+          currentDay={MOCKED_CURRENT_DAY}
+          selectedFood={null}
+          user={MOCKED_USER}
+        />
+      );
+      const linkElement = screen.getByRole("link");
+      expect(linkElement).toHaveAttribute("href", "/today");
+    });
   });
 });
