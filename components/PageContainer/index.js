820fa2c12a900e55aeb6bbee2c6be15bdede5c9a
Gino Ochoa <gochoa1088@gmail.com>
2022-09-12 13:19:23 -0700

    implement PageContainer tests

--- components/PageContainer/index.js
+++ components/PageContainer/index.js
@@ -2,7 +2,10 @@ import styles from "./styles.module.css";
 
 const PageContainer = ({ children, isLanding = false }) => {
   return (
-    <div className={isLanding ? styles.containerLanding : styles.container}>
+    <div
+      className={isLanding ? styles.containerLanding : styles.container}
+      role="pageContainer"
+    >
       {children}
     </div>
   );
