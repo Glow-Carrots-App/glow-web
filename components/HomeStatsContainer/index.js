30f1e97d39cf6d04fdbd8d358be9fc8e3a3b60a7
Gino Ochoa <gochoa1088@gmail.com>
2022-09-12 12:39:11 -0700

    stats refactor

--- components/HomeStatsContainer/index.js
+++ components/HomeStatsContainer/index.js
@@ -4,17 +4,9 @@ const HomeStatsContainer = () => {
   return (
     <>
       <div className={styles.statsContainer} role="statsContainer">
-        <img
-          className={styles.stats1}
-          src="/landing/glow-stats1-home.png"
-          role="stats1"
-        />
+        <img className={styles.stats1} src="/landing/glow-stats1-home.png" />
         <p className={styles.statsText}>Keep track of statistics and more!</p>
-        <img
-          className={styles.stats2}
-          src="/landing/glow-stats2-home.png"
-          role="stats2"
-        />
+        <img className={styles.stats2} src="/landing/glow-stats2-home.png" />
       </div>
     </>
   );
--- components/HomeStatsContainer/index.test.js
+++ components/HomeStatsContainer/index.test.js
@@ -2,8 +2,10 @@ import { render, screen } from "@testing-library/react";
 
 import HomeStatsContainer from ".";
 
-const STATS1 = "/landing/glow-stats1-home.png";
-const STATS2 = "/landing/glow-stats2-home.png";
+const STATS_SRC = [
+  "/landing/glow-stats1-home.png",
+  "/landing/glow-stats2-home.png",
+];
 
 describe("HomeStatsContainer component", () => {
   beforeEach(() => render(<HomeStatsContainer />));
@@ -14,16 +16,14 @@ describe("HomeStatsContainer component", () => {
   });
 
   describe("statsContainer", () => {
-    it("should render stats1 image", () => {
-      const stats1 = screen.getByRole("stats1");
-      expect(stats1).toBeInTheDocument();
-      expect(stats1).toHaveAttribute("src", STATS1);
+    it("should render 2 stats images", () => {
+      const stats = screen.getAllByRole("img");
+      stats.forEach((stat, i) => {
+        expect(stat).toBeInTheDocument();
+        expect(stat).toHaveAttribute("src", STATS_SRC[i]);
+      });
     });
-    it("should render stats2 image", () => {
-      const stats2 = screen.getByRole("stats2");
-      expect(stats2).toBeInTheDocument();
-      expect(stats2).toHaveAttribute("src", STATS2);
-    });
+
     it("should render stats text", () => {
       const statsText = screen.getByText(/Keep track of statistics and more!/);
       expect(statsText).toBeInTheDocument();
