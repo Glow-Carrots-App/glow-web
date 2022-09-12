3a21ed6eaf42336db790a854216588bfb5fad222
Gino Ochoa <gochoa1088@gmail.com>
2022-09-12 11:57:42 -0700

    HomeGraphContainer tests

--- components/HomeGraphContainer/index.js
+++ components/HomeGraphContainer/index.js
@@ -3,8 +3,10 @@ import styles from "./styles.module.css";
 const HomeGraphContainer = () => {
   return (
     <>
-      <div className={styles.graphContainer}>
-        <p className={styles.graphText}>Data driven graphs!</p>
+      <div className={styles.graphContainer} role="graphContainer">
+        <p className={styles.graphText} role="graphText">
+          Data driven graphs!
+        </p>
         <img className={styles.graphImage} src="/landing/glow-graph-home.png" />
       </div>
     </>
