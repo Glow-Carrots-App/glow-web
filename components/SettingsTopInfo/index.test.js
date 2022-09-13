d902e07abb372c8541756394e8e28c70006b36ba
Gino Ochoa <gochoa1088@gmail.com>
2022-09-12 17:12:58 -0700

    test refactor

--- components/SettingsTopInfo/index.test.js
+++ components/SettingsTopInfo/index.test.js
@@ -6,30 +6,25 @@ const USER = {
   avatar: "/avatar.jpg",
 };
 
+const HREFS = ["/profile", "/settings/change-avatar"];
+const IMG_SRCS = ["/buttonIcons/back.png", USER.avatar];
+
 describe("SettingsTopInfo component", () => {
   beforeEach(() => render(<SettingsTopInfo user={USER} />));
 
-  it("should render an anchor1 with /profile href", () => {
-    const anchorElement = screen.getByRole("anchor1");
-    expect(anchorElement).toBeInTheDocument();
-    expect(anchorElement).toHaveAttribute("href", "/profile");
+  it("should render anchors with correct hrefs", () => {
+    const anchorElements = screen.getAllByRole("link");
+    anchorElements.forEach((anchor, i) => {
+      expect(anchor).toBeInTheDocument();
+      expect(anchor).toHaveAttribute("href", HREFS[i]);
+    });
   });
 
-  it("should render an image1 with correct src", () => {
-    const imageElement = screen.getByRole("image1");
-    expect(imageElement).toBeInTheDocument();
-    expect(imageElement).toHaveAttribute("src", "/buttonIcons/back.png");
-  });
-
-  it("should render an anchor2 with /settings/change-avatar href", () => {
-    const anchorElement = screen.getByRole("anchor2");
-    expect(anchorElement).toBeInTheDocument();
-    expect(anchorElement).toHaveAttribute("href", "/settings/change-avatar");
-  });
-
-  it("should render an image2 with correct src", () => {
-    const imageElement = screen.getByRole("image2");
-    expect(imageElement).toBeInTheDocument();
-    expect(imageElement).toHaveAttribute("src", USER.avatar);
+  it("should render images with correct src", () => {
+    const imageElements = screen.getAllByRole("img");
+    imageElements.forEach((image, i) => {
+      expect(image).toBeInTheDocument();
+      expect(image).toHaveAttribute("src", IMG_SRCS[i]);
+    });
   });
 });
