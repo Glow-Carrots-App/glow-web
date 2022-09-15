b6e224132b3c7537943398b9c7a725d45f8bb30c
sweetnectar <93552075+sweetnectar@users.noreply.github.com>
2022-09-15 14:40:42 -0700

    First commit

---
+++ pages/settings/about/index.test.js
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+
+import About from ".";
+
+import { render, screen } from "@testing-library/react";
+
+import About from ".";
+
+describe("About page", () => {
+  beforeEach(() => render(<About />));
+  it("should render the first paragraph tag", () => {
+    const paragraphElement = screen.getByText(
+      /Glow is a food tracker app inspired by/i
+    );
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  it("should render the second paragraph tag", () => {
+    const paragraphElement = screen.getByText(
+      /This app is intended for all ages/i
+    );
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  it("should render the third paragraph tag", () => {
+    const paragraphElement = screen.getByText(/Icons are made by/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  it("should render a Freepik anchor tag", () => {
+    const anchorElement = screen.getByText(/Freepik/i);
+    expect(anchorElement).toBeInTheDocument();
+  });
+
+  it("should render a www.flaticon.com anchor tag", () => {
+    const anchorElement = screen.getByText(/www.flaticon.com/);
+    expect(anchorElement).toBeInTheDocument();
+  });
+});
